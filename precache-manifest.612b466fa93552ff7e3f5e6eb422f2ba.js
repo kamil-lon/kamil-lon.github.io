040c4594d6717c8778100be89cf20159a06cd3ff
Kamiel <kamil-lon@wp.pl>
2019-02-24 01:57:42 +0100

    Updates

---
+++ precache-manifest.612b466fa93552ff7e3f5e6eb422f2ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36d685a046ac78654aa4",
+    "url": "/game2/static/js/runtime~main.36d685a0.js"
+  },
+  {
+    "revision": "d5b957b05fda089382dc",
+    "url": "/game2/static/js/main.d5b957b0.chunk.js"
+  },
+  {
+    "revision": "00820d71b6ea884c2da8",
+    "url": "/game2/static/js/2.00820d71.chunk.js"
+  },
+  {
+    "revision": "d5b957b05fda089382dc",
+    "url": "/game2/static/css/main.648d23ab.chunk.css"
+  },
+  {
+    "revision": "f8c0a46c0d5b1f4b9110f39d402004b5",
+    "url": "/game2/index.html"
+  }
+];
--- src/components/Header.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import "./Header.css"
-
-const Header = () => <div className='header'>game2</div>
-
-export default Header
